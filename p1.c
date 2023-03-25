c0fdf9e060a437f017c958903ae2e00a8e9aa0ad
Chirag S <chirags.221cs214@gmail.com>
2023-03-25 20:12:12 +0530

    Real stuffs added

--- p1.c
+++ p1.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 printf("Hello Chirag ");
+char s[10];
+printf("Enter the name ");
+gets(s);
+printf("hello %s",s);
 printf("Hello world ...whatsUPP..Hw have You Been ");
 }
